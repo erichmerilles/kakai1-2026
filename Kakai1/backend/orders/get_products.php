74dc4473d5c443b4528769070cad9a7989c0078e
erichmerilles <erich.merilles@gmail.com>
2026-02-19 16:07:47 +0800

    Delete Kakai1 directory

--- Kakai1/backend/orders/get_products.php
+++
@@ -1,9 +0,0 @@
-<?php
-header('Content-Type: application/json');
-require_once __DIR__ . '/../../config/db.php';
-
-// products table in your dump has columns product_id, product_name, price, stock
-$res = $conn->query("SELECT product_id, product_name, price, stock FROM products ORDER BY product_name ASC");
-$data = $res ? $res->fetch_all(MYSQLI_ASSOC) : [];
-echo json_encode(['success'=>true,'data'=>$data]);
-?>
--- Kakai1/backend/utils/hash_password.php
+++
@@ -1,5 +0,0 @@
-<?php
-$password = 'admin123'; // ← change this to the password you want
-$hash = password_hash($password, PASSWORD_DEFAULT);
-echo "Hashed password: " . $hash;
-?>
